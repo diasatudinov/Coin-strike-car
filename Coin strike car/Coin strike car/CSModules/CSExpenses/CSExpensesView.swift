156ffccbcd18e47ce9dcf2f1457defa9f3aa186f
Dias <dias.atudinov@gmail.com>
2025-11-29 03:35:27 +0500

    new files

---
+++ Coin strike car/Coin strike car/CSModules/CSExpenses/CSExpensesView.swift
@@ -0,0 +1,18 @@
+//
+//  CSExpensesView.swift
+//  Coin strike car
+//
+//  Created by Dias Atudinov on 29.11.2025.
+//
+
+import SwiftUI
+
+struct CSExpensesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CSExpensesView()
+}
---
+++ Coin strike car/Coin strike car/CSModules/Home/Views/CSAddSpendingView.swift
@@ -0,0 +1,18 @@
+//
+//  CSAddSpendingView.swift
+//  Coin strike car
+//
+//  Created by Dias Atudinov on 28.11.2025.
+//
+
+import SwiftUI
+
+struct CSAddSpendingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CSAddSpendingView()
+}
